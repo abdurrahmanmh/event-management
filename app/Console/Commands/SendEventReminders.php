77759d43df099cf82ca9da94ffaf4d18aa2bff66
Abdurrahman <arahmanmh15@gmail.com>
2025-01-06 16:16:30 +0800

    19 Reminding about event custom artisan command

---
+++ app/Console/Commands/SendEventReminders.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Event;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class SendEventReminders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-event-reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends all event reminders to attendees if near 24hours to event start time';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $events = Event::with('attendees.user')
+            ->whereBetween('start_time', [now(), now()->addDay()])
+            ->get();
+
+        $eventCount = $events->count();
+        $eventLabel = Str::plural('event', $eventCount);
+
+        $this->info("Found {$eventCount} {$eventLabel} to remind attendees");
+
+        $events->each(fn($event)
+            => $event->attendees->each(fn($attendee)
+                => $this->info("Sending reminder to {$attendee->user->id} for {$event->name}")));
+
+        $this->info('Reminder Notification Sent Successfully');
+    }
+}
